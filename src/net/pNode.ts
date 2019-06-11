db10bc9544ef5a3f3b68939bc3edf73b700b0196
tangm <gary@gaia.world>
2019-06-11 14:08:52 +0800

    the pNode struct

--- src/net/pNode.ts
+++ src/net/pNode.ts
@@ -23,19 +23,26 @@ interface NetMsg {
  * 定义了所有的消息类型
  */
 interface MSG_TYPE {
-
+    VERSION : 0,
+    VERACK,
+    ADDR,
+    GETADDR,
+    INV,//announce new tx or blocks, or reply to a getblocks message or mempool message
+    GETDATA,//还有个字段用于表示是需要获取TX详细信息还是block详细信息
+    MERKLEBLOCK,//在demo版本中暂时不适用该字段
+    GETHEADERS,//The getheaders message requests a headers message that provides block headers starting from a particular point in the block chain.
+    TX, 
 }
 
-enum INV_MSG_TYPE
-{
-    UNDEFINED = 0,
-    MSG_TX = 1,
-    MSG_BLOCK = 2,
-}
+interface INV_MSG_TYPE {
+    UNDEFINED:0,
+    MSG_TX:1,
+    MSG_BLOCK:2
+} 
 
 interface Inv {
     hash: number;
-    MsgType: INV_MSG_TYPE;//实际取值只会是
+    MsgType: INV_MSG_TYPE;//实际取值只会是TX和BLOCK
 }
 export class PNode { 
     nNodeType:NODE_TYPE;
