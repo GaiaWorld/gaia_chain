bf10ee54b399784afd3c91c48f51e969666f4cf5
jayphbee <jayphbee@163.com>
2019-06-11 10:41:23 +0800

    seperate config

---
+++ src/params/committee.ts
@@ -0,0 +1,16 @@
+/**
+ * committee config
+ */
+
+// tslint:disable-next-line:variable-name
+export const CommitteeConfig =  {
+    // minium tokens to become a forger
+    MIN_TOKEN: 1000,
+    // the default time for create a new block. For the main chain it is 3000 millisecond.
+    FORGE_TIME: 3 * 1000,
+    // maximum height for one forger to accumulate weight priority
+    MAX_ACCUMULATED_HEIGHT: 150000,
+
+    // stake can withdraw after a certain blocks
+    WITHDRAW_RESERVE_BLOCKS: 256000
+};
