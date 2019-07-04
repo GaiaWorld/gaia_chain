341a7bc710497683bd73df56e156520211b4fe35
jayphbee <jayphbee@163.com>
2019-07-04 18:25:29 +0800

    add genesis block random

--- src/params/genesis.ts
+++ src/params/genesis.ts
@@ -1,3 +1,7 @@
 export const GENESIS = {
-    hash: 'f80ac0800aeba0e82ed53a4451527223fe3726e6'
+    hash: 'f80ac0800aeba0e82ed53a4451527223fe3726e6',
+    blockRandom: 'cc6c85a369f741fd6f409627a0f73fd166f7dba6ba1b5be6c55703bb5243e013',
+    allocs: [
+        
+    ]
 };
