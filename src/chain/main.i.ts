32301c15ad89f2add796c8833c2abfebc7d3ce86
jayphbee <jayphbee@163.com>
2019-07-05 14:33:28 +0800

    fix bugs

--- src/chain/main.i.ts
+++ src/chain/main.i.ts
@@ -1,44 +1,19 @@
 /**
  * main function
  */
-import { calcTxHash, getTipHeight } from '../chain/blockchain';
+import { getTipHeight } from '../chain/blockchain';
 import { INV_MSG_TYPE } from '../net/msg';
 import { Inv } from '../net/server/rpc.s';
 import { notifyNewBlock, notifyNewTx } from '../net/server/subscribe';
 import { setTimer } from '../util/task';
-import { buildForgerCommittee, generateTxs } from '../util/test_helper';
-import { bestWeightAddr, generateBlock, getCommitteeConfig, getMiningConfig, increaseWeight, isSyncing, newBlockChain } from './blockchain';
+import { isSyncing, newBlockChain } from './blockchain';
 
 const start = (): void => {
     newBlockChain();
-    buildForgerCommittee();
     if (!isSyncing()) {
         setTimer(() => {
-            increaseWeight();
-            console.log('\n==========================================================================================\n');
-            const miningCfg = getMiningConfig();
-            const cc = getCommitteeConfig();
-            const bestAddr = bestWeightAddr();
-            console.log('maxGroupNumber: ---------------- ', cc.maxGroupNumber);
-            if (getTipHeight() % cc.maxGroupNumber === miningCfg.groupNumber) {
-                if (bestAddr === miningCfg.beneficiary) {
-                    const block = generateBlock();
-                    console.log('\n\ngenerate new block hash  : ------------ ', block.body.headerHash);
-                    const invmsg = new Inv();
-                    invmsg.hash = block.body.headerHash;
-                    invmsg.height = block.header.height;
-                    invmsg.MsgType = INV_MSG_TYPE.MSG_BLOCK;
-
-                    // const newTxs = generateTxs(1);
-                    // const invmsg2 = new Inv();
-                    // invmsg2.hash = calcTxHash(newTxs[0]);
-                    // invmsg2.height = 0;
-                    // invmsg2.MsgType = INV_MSG_TYPE.MSG_TX;
-
-                    notifyNewBlock(invmsg);
-                    // notifyNewTx(invmsg2);
-                }
-            }
+            // TODO
+            console.log('xxxxxxxxx');
         }, null, 5000);
     }
     
--- src/util/test_helper.ts
+++ src/util/test_helper.ts
@@ -2,49 +2,12 @@
  * test helpers
  */
 
-import { calcTxHash } from '../chain/blockchain';
-import { CommitteeConfig, Forger, ForgerCommittee, Transaction } from '../chain/schema.s';
+import { Forger, ForgerCommittee, Transaction } from '../chain/schema.s';
 import { signTx } from '../chain/transaction';
 import { deriveInitWeight } from '../consensus/committee';
 import { GENESIS } from '../params/genesis';
-import { buf2Hex, genKeyPairFromSeed, getRand, hex2Buf, pubKeyToAddress, sha256 } from './crypto';
+import { buf2Hex, genKeyPairFromSeed, getRand, hex2Buf, pubKeyToAddress } from './crypto';
 import { persistBucket } from './db';
-
-export const buildForgerCommittee = (): void => {
-    const bkt = persistBucket(ForgerCommittee._$info.name);
-    if (bkt.get<string, [ForgerCommittee]>('FC')[0]) {
-        return;
-    }
-
-    const bkt2 = persistBucket(CommitteeConfig._$info.name);
-    const cc = bkt2.get<string, [CommitteeConfig]>('CC')[0];
-
-    const forgerPerGroup = 2;
-    const fc = new ForgerCommittee();
-    fc.groups = [];
-    fc.waitsForAdd = new Map();
-    fc.waitsForRemove = new Map();
-    
-    for (let i = 0; i < cc.maxGroupNumber; i++) {
-        fc.groups.push([]);
-
-        for (let j = 0; j < forgerPerGroup; j++) {
-            const fg = new Forger();
-            fg.address = pubKeyToAddress(sha256(j.toString() + i.toString()));
-            fg.pk = j.toString();
-            fg.pubKey = sha256(j.toString() + i.toString());
-            fg.groupNumber = i;
-            fg.initWeigth = getRand(1)[0] * getRand(1)[0];
-            fg.lastHeight = 0;
-            fg.lastWeight = 0;
-            fg.stake = getRand(1)[0] * getRand(1)[0] + 10000;
-
-            fc.groups[i].push(fg);
-        }
-    }
-
-    bkt.put('FC', fc);
-};
 
 export const generateTxs = (len: number): Transaction[] => {
     const txBkt = persistBucket(Transaction._$info.name);
@@ -88,16 +51,16 @@ export const generateMiners = (len: number): void => {
             const [privKey, pubKey] = genKeyPairFromSeed(getRand(32));
             const address = pubKeyToAddress(pubKey);
             const groupNumber = parseInt(address.slice(address.length - 2), 16);
-            const stake = new DataView(getRand(4).buffer).getUint32(0) % 10000 + 10000;
+            const stake = new DataView(getRand(4).buffer).getUint32(0) % 100000 + 10000;
             if (groupNumber === i) {
                 count += 1;
-                // console.log('{');
-                // console.log('address: ', `'${address}',`);
-                // console.log('pubKey: ', `'${buf2Hex(pubKey)}',`);
-                // console.log('privKey: ', `'${buf2Hex(privKey)}',`);
-                // console.log('stake: ', stake);
-                // console.log('},');
-                // console.log('\n');
+                console.log('{');
+                console.log('address: ', `'${address}',`);
+                console.log('pubKey: ', `'${buf2Hex(pubKey)}',`);
+                console.log('privKey: ', `'${buf2Hex(privKey)}',`);
+                console.log('stake: ', stake);
+                console.log('},');
+                console.log('\n');
 
                 const forger = new Forger();
                 forger.address = address;
