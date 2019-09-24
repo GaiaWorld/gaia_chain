f32ee7a6e74f3948e5d2482d53b55dd52864ab04
jayphbee <jayphbee@163.com>
2019-09-24 10:55:57 +0800

    get fork chain

--- src/chain/fork_manager.ts
+++ src/chain/fork_manager.ts
@@ -1,17 +1,55 @@
 // fork manager
 import { Tr as Txn } from '../pi/db/mgr';
-import { ChainHead } from './schema.s';
+import { DEFAULT_FILE_WARE } from '../pi_pt/constant';
+import { buf2Hex, number2Uint8Array } from '../util/crypto';
+import { Logger, LogLevel } from '../util/logger';
+import { BestForkChain, ForkChain, ForkPoint, Header } from './schema.s';
 
-export const getForkChainId = (txn: Txn, height: number, hash: string): number => {
-    // read forger in forger committee
-    return 0;
+const logger = new Logger('FORK_MANAGER', LogLevel.DEBUG);
+
+export const getForkChainId = (txn: Txn, header: Header): number => {
+    const fork = txn.query(
+        [{ ware: DEFAULT_FILE_WARE
+            , tab: ForkPoint._$info.name
+            , key: `${header.prevHash}${buf2Hex(number2Uint8Array(header.height - 1))}`}]
+            , 1000
+            , false
+        );
+
+    if (fork) {
+        return (<ForkPoint>fork[0].value).forkChainId;
+    }
+
+    logger.warn(`Not found appropriate chain id for header hash ${header.bhHash} height ${header.height}`);
 };
 
-export const getChainHeadForChainId = (txn: Txn, height: number, hash: string): ChainHead => {
-    return;
+export const getForkChain = (txn: Txn, chainId: number): ForkChain => {
+    const forkChain = txn.query(
+        [{ ware: DEFAULT_FILE_WARE, tab: ForkChain._$info.name, key: chainId }]
+        , 1000
+        , false
+    );
+
+    if (forkChain) {
+        return <ForkChain>forkChain[0].value;
+    }
+    logger.warn(`Can not found fork chain for chain id ${chainId}`);
 };
 
 // canonical chain is the highest voting power chain
-export const getCanonicalChain = (txn: Txn): ChainHead => {
-    return;
+export const getCanonicalForkChain = (txn: Txn): ForkChain => {
+    const iter = txn.iter_raw(DEFAULT_FILE_WARE, BestForkChain._$info.name, undefined, true, '');
+    const best = iter.next();
+
+    if (best) {
+        const forkChain = txn.query(
+            [{ ware: DEFAULT_FILE_WARE, tab: ForkChain._$info.name, key: (<BestForkChain>best[1]).forkChainId }]
+            , 1000
+            , false
+        );
+        if (forkChain) {
+            return <ForkChain>forkChain[0].value;
+        }
+    }
+    logger.error(`Can not get canonical chain`);
 };
