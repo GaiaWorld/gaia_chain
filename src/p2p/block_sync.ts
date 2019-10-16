e5e694afe2e9f4548ca740967c1d4b3e13b6b741
jayphbee <jayphbee@163.com>
2019-10-16 18:05:59 +0800

    add p2p module

---
+++ src/p2p/block_sync.ts
@@ -0,0 +1,23 @@
+/**
+ * block syncronizer
+ */
+
+// sync header if we don't catch up with peer node
+export const fetchHeader = (): void => {
+    return;
+};
+
+// sync block if we don't catch up with peer
+export const fetchBlock = (): void => {
+    return;
+};
+
+// notify peer new tx
+export const notifyPeerNewTx = (): void => {
+    return;
+};
+
+// notify peer new block hash
+export const notifyPeerNewBlockHash = (): void => {
+    return;
+};
---
+++ src/p2p/peer_list.ts
@@ -0,0 +1,11 @@
+/**
+ * peer list
+ */
+
+export const DEFAULT_PEER = [
+    '127.0.0.1:2001',
+    '127.0.0.1:2002',
+    '127.0.0.1:2003'
+];
+
+export const OWN_NET_ADDR = '127.0.0.1:2001';
